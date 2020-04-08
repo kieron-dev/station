b494974c0afb0948d12bc854d83fe52a6bc748ae
Danail Branekov <danailster@gmail.com>
2020-04-08 14:12:43 +0300

    Pin cf-cli to 6.49
    There is some issue with cf-cli 6.50 when running CATs - log capturing
    does not work well and tests are failing without reason

--- provision.sh
+++ provision.sh
@@ -100,8 +100,8 @@ install_cf_cli(){
   wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
   echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
   # ...then, update your local package index, then finally install the cf CLI
-  sudo apt-get update
-  sudo apt-get install cf-cli
+  apt-get update
+  apt-get -y install cf-cli=6.49.0
 }
 
 install_misc_tools(){
