9a82d59cfc0d541255f086ec0786ac29e46cebeb
Giuseppe Capizzi <gcapizzi@pivotal.io>
2020-04-22 10:16:32 +0000

    Always run curl silently
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- provision.sh
+++ provision.sh
@@ -93,7 +93,7 @@ install_nvim() {
 
 install_gcloud_cli() {
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  curl -sL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
   apt-get update
   apt-get -y install google-cloud-sdk
 }
@@ -125,7 +125,7 @@ install_diff-so-fancy() {
 }
 
 install_k14s_tools() {
-  curl -L https://k14s.io/install.sh | bash
+  curl -sL https://k14s.io/install.sh | bash
 }
 
 main
