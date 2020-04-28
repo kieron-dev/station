e4e7c4775ea71da592c4b908a9c8e45a50ed3c84
Julian Skupnjak <skupnjak@de.ibm.com>
2020-04-28 13:47:53 +0100

    match golangci-lint version to that in CI
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>

--- provision-user.sh
+++ provision-user.sh
@@ -36,7 +36,7 @@ setup_helm_client() {
 
 install_gotools() {
   echo ">>> Installing golangci-lint"
-  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$HOME/go/bin" v1.24.0
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$HOME/go/bin" v1.25.1
 
   echo ">>> Installing gopls"
   GO111MODULE=on go_get golang.org/x/tools/gopls@latest
