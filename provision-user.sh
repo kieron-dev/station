5c9770b564a3b8eb4a3aeb10af87257ca0f874ac
Georgi Sabev <georgethebeatle@gmail.com>
2021-01-13 12:33:53 +0000

    Bump golangci-lint

--- provision-user.sh
+++ provision-user.sh
@@ -72,7 +72,7 @@ install_ibmcloud_cli() {
 
 install_gotools() {
   echo ">>> Installing golangci-lint"
-  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$HOME/go/bin/" v1.34.1
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$HOME/go/bin/" v1.35.2
 
   echo ">>> Installing gopls"
   GO111MODULE=on go_get golang.org/x/tools/gopls@latest
