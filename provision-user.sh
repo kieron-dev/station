431e68e32b9c51330e459502d60cf3625a9623a7
Danail Branekov <danailster@gmail.com>
2021-03-04 13:49:27 +0200

    Bump kind to 0.10.0
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- provision-user.sh
+++ provision-user.sh
@@ -250,8 +250,8 @@ install_misc_tools() {
   echo ">>> Installing k9s (v0.21.4)"
   curl -L https://github.com/derailed/k9s/releases/download/v0.21.4/k9s_Linux_x86_64.tar.gz | tar xvzf - -C "$HOME/bin" k9s
 
-  echo ">>> Installing kind (v0.8.1)"
-  curl -L https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-linux-amd64 -o "$HOME/bin/kind"
+  echo ">>> Installing kind (v0.10.0)"
+  curl -L https://github.com/kubernetes-sigs/kind/releases/download/v0.10.0/kind-linux-amd64 -o "$HOME/bin/kind"
   chmod +x "$HOME/bin/kind"
 
   echo ">>> Installing promql-cli (v0.2.1)"
