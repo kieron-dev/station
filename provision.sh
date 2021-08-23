3408dd5c07c9744730aaf3951046b74f9c29010f
Kieron Browne <kbrowne@vmware.com>
2021-08-23 15:52:13 +0100

    Bump golang to 1.17
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- provision.sh
+++ provision.sh
@@ -164,7 +164,7 @@ install_golang() {
   echo ">>> Installing Golang"
   rm -rf /usr/local/go
   mkdir -p /usr/local/go
-  curl -sL "https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz" | tar xz -C "/usr/local"
+  curl -sL "https://dl.google.com/go/go1.17.linux-amd64.tar.gz" | tar xz -C "/usr/local"
 }
 
 install_nodejs() {
