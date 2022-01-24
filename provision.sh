c240d4b1433b1de927e596bd85e53d020ae54b26
Giuseppe Capizzi <gcapizzi@pivotal.io>
2022-01-24 11:52:49 +0000

    Remove temporary binary

--- provision.sh
+++ provision.sh
@@ -158,6 +158,7 @@ install_snaps() {
 install_kubectl() {
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  rm -f kubectl
 }
 
 install_golang() {
