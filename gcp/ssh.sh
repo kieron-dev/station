be7dba2399d8f90ae08da455c918167f1f5aa8d8
Georgi Sabev <georgethebeatle@gmail.com>
2022-04-29 23:23:17 +0300

    Handle some errors in a user friendly way in station scripts

--- gcp/ssh.sh
+++ gcp/ssh.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-ip=$(
-  gcloud compute instances describe "${EIRINI_STATION_USERNAME}-eirini-station" \
-    --zone="europe-west2-a" \
-    --project="cff-eirini-peace-pods" \
-  | grep natIP | cut -d ":" -f 2 | xargs
+description=$(
+  gcloud compute instances describe georgethebeatle-eirini-station \
+    --project cff-eirini-peace-pods \
+    --zone="europe-west2-a"
 )
+status=$(yq '.status' <<<"$description")
+ip=$(yq '.networkInterfaces[0].accessConfigs[0].natIP' <<<"$description")
+
+if [[ "$status" != "RUNNING" ]]; then
+  echo "Station is not runninng. Run 'station start' to start it"
+  exit 1
+fi
 
 ssh \
   -A \
