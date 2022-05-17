98c2655220e3b357c6aac635fcac1e9c6e08f058
Giuseppe Capizzi <gcapizzi@pivotal.io>
2022-05-17 12:14:36 +0000

    Switch to e2-highcpu-8 machines on GCP

--- gcp/create.sh
+++ gcp/create.sh
@@ -5,7 +5,7 @@ gcloud compute instances create "$EIRINI_STATION_USERNAME-eirini-station" \
   --project="cff-eirini-peace-pods" \
   --image-project="ubuntu-os-cloud" \
   --image-family="ubuntu-2004-lts" \
-  --machine-type="n1-standard-8" \
+  --machine-type="e2-highcpu-8" \
   --boot-disk-size="100GB" \
   --boot-disk-type="pd-ssd" \
   --zone="europe-west2-a"
