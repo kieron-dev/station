885abde5ff431f5fbf29be7a0cdd5fbc3dbcc18f
Danail Branekov <danailster@gmail.com>
2022-06-10 13:58:09 +0000

    install kubectl buildkit
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- provision.sh
+++ provision.sh
@@ -160,6 +160,9 @@ install_kubectl() {
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
   rm -f kubectl
+
+  curl -sL https://github.com/vmware-tanzu/buildkit-cli-for-kubectl/releases/download/v0.1.5/linux-v0.1.5.tgz |
+    tar -C /usr/local/bin -xzf -
 }
 
 install_golang() {
