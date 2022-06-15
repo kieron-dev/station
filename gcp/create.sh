886a33ce27e5d06328fcae7bbc6a6ccac3dcf2ae
Danail Branekov <danailster@gmail.com>
2022-06-15 15:49:00 +0300

    Bump machine memory to 16G
    ... because it becomes unusable when running Kirifi tests

--- gcp/create.sh
+++ gcp/create.sh
@@ -10,7 +10,7 @@ gcloud compute instances create "$EIRINI_STATION_USERNAME-eirini-station" \
   --project="cff-eirini-peace-pods" \
   --image-project="ubuntu-os-cloud" \
   --image-family="ubuntu-2204-lts" \
-  --machine-type="e2-highcpu-8" \
+  --machine-type="e2-custom-8-16384" \
   --boot-disk-size="100GB" \
   --boot-disk-type="pd-ssd" \
   --zone="europe-west2-a" \
