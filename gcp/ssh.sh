c18ff65233864b10b1008980680fe0fab17063c7
Georgi Sabev <georgethebeatle@gmail.com>
2022-09-05 11:52:05 +0300

    Retry the ssh command to the station
    After station is started in the morning it can take some seconds for the
    ssh service to become ready, which means that the ssh script will fail
    and will have to be retried manually a couple of times. This commit
    automates these manual retries.

--- gcp/ssh.sh
+++ gcp/ssh.sh
@@ -9,8 +9,17 @@ if [[ "$STATION_STATUS" != "RUNNING" ]]; then
   exit 1
 fi
 
-ssh \
-  -A \
-  -R "/home/${EIRINI_STATION_USERNAME}/.gnupg/S.gpg-agent-host:$(gpgconf --list-dirs agent-socket)" \
-  -o "UserKnownHostsFile=/dev/null" \
-  "${EIRINI_STATION_USERNAME}@$STATION_IP"
+for attempt in $(seq 10); do
+  if ssh \
+    -A \
+    -R "/home/${EIRINI_STATION_USERNAME}/.gnupg/S.gpg-agent-host:$(gpgconf --list-dirs agent-socket)" \
+    -o "UserKnownHostsFile=/dev/null" \
+    "${EIRINI_STATION_USERNAME}@${STATION_IP}"; then
+    exit 0
+  fi
+
+  sleep 1
+done
+
+echo "Unable to ssh to the station after $attempt attempts"
+exit 1
