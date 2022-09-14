5a7d47a1def8e427c0b5fce33c2cc060be70c127
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-09-14 13:18:28 +0200

    Fix typo

--- gcp/ssh.sh
+++ gcp/ssh.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
 if [[ "$STATION_STATUS" != "RUNNING" ]]; then
-  echo "Station is not runninng. Run 'station start' to start it"
+  echo "Station is not running. Run 'station start' to start it"
   exit 1
 fi
 
