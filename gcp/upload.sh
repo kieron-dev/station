cf3b8c032eecba3cc366b353329bbe4bca02dc9b
Giuseppe Capizzi <g.capizzi@gmail.com>
2022-09-16 13:41:45 +0200

    Add download and upload scripts to the GCP station

---
+++ gcp/upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+source "$SCRIPT_DIR/common.sh"
+
+if [[ "$STATION_STATUS" != "RUNNING" ]]; then
+  echo "Station is not running. Run 'station start' to start it"
+  exit 1
+fi
+
+scp \
+  -A \
+  -o "UserKnownHostsFile=/dev/null" \
+  "$1" "${EIRINI_STATION_USERNAME}@${STATION_IP}:$2" 
