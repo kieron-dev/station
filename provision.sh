f42306d761ab0f8b6029fa42f0c021b89c258e9e
Danail Branekov <danailster@gmail.com>
2022-12-07 09:35:14 +0000

    Bump golang to 1.19.4
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- provision.sh
+++ provision.sh
@@ -189,7 +189,7 @@ install_golang() {
   echo ">>> Installing Golang"
   rm -rf /usr/local/go
   mkdir -p /usr/local/go
-  curl -sL "https://dl.google.com/go/go1.19.linux-amd64.tar.gz" | tar xz -C "/usr/local"
+  curl -sL "https://dl.google.com/go/go1.19.4.linux-amd64.tar.gz" | tar xz -C "/usr/local"
 }
 
 install_nodejs() {
