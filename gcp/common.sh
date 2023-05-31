1a9d4490adaa48c35e0316e8a2c376edaae98684
Kieron Browne <kbrowne@pivotal.io>
2023-05-31 11:27:46 +0100

    Make this work on both linux and mac
    For some reason the separators vary between tabs and spaces in the
    gcloud output. Use awk to make the different disappear.

--- gcp/common.sh
+++ gcp/common.sh
@@ -1,11 +1,11 @@
-info=$(
+info="$(
   gcloud compute instances describe "${EIRINI_STATION_USERNAME}-eirini-station" \
     --project cf-on-k8s-wg \
     --zone="europe-west2-a" \
     --format="value(status, networkInterfaces[0].accessConfigs[0].natIP, name)"
-)
+)"
 
-export STATION_STATUS=$(cut -f 1 -d ' ' <<<$info)
-export STATION_IP=$(cut -f 2 -d ' ' <<<$info)
-export STATION_NAME=$(cut -f 3 -d ' ' <<<$info)
+export STATION_STATUS="$(awk '{print $1}' <<<$info)"
+export STATION_IP="$(awk '{print $2}' <<<$info)"
+export STATION_NAME="$(awk '{print $3}' <<<$info)"
 export STATION_HISTORY_BACKUP="$HOME/eirini-station-history-backup"
