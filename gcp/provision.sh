6cfb5c96452fac359f4bb9b79d38ee3b7a5f7ed9
Giuseppe Capizzi <g.capizzi@gmail.com>
2023-06-12 11:53:23 +0100

    Stop forwarding the GPG socket in provision.sh

--- gcp/provision.sh
+++ gcp/provision.sh
@@ -6,8 +6,8 @@ source "$SCRIPT_DIR/common.sh"
 
 echo "Provisioning $EIRINI_STATION_USERNAME-eirini-station"
 scp -r ~/workspace/eirini-station/provision*.sh "$EIRINI_STATION_USERNAME@$STATION_IP:/tmp"
-ssh -A -R $HOME/.gnupg/S.gpg-agent-guest:$(gpgconf --list-dirs agent-socket) "$EIRINI_STATION_USERNAME@$STATION_IP" "sudo /tmp/provision.sh"
-ssh -A -R $HOME/.gnupg/S.gpg-agent-guest:$(gpgconf --list-dirs agent-socket) "$EIRINI_STATION_USERNAME@$STATION_IP" "/tmp/provision-user.sh"
+ssh -A "$EIRINI_STATION_USERNAME@$STATION_IP" "sudo /tmp/provision.sh"
+ssh -A "$EIRINI_STATION_USERNAME@$STATION_IP" "/tmp/provision-user.sh"
 
 if [[ -f "$STATION_HISTORY_BACKUP" ]]; then
   scp -r "$STATION_HISTORY_BACKUP" "$EIRINI_STATION_USERNAME@$STATION_IP:~/.zsh_history"
